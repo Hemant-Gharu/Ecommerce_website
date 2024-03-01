19eb4f25887a284b7a70147212348b5e2209fafd
Hemant Gharu <hemantgharu98@gmail.com>
2024-03-01 21:14:43 +0530

    code correction

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,15 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-<<<<<<< HEAD
-=======
-import data from "./data";
->>>>>>> ea70d730c0eeac744a98985b3e7e075e7913eec5
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Hero />
-<<<<<<< HEAD
       {/* <Footer/> */}
-=======
-      <Footer />
->>>>>>> ea70d730c0eeac744a98985b3e7e075e7913eec5
+ac744a98985b3e7e075e7913eec5
     </div>
   );
 }
-
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> ea70d730c0eeac744a98985b3e7e075e7913eec5
