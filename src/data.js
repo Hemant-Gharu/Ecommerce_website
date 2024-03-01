729aa523c981cbc4832f050b3218c1a936b13bdf
Hemant Gharu <hemantgharu98@gmail.com>
2024-03-01 21:12:58 +0530

    images added

--- src/data.js
+++ src/data.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-// const data = [
-//     { product: "Apple Macbook Air 13", price: 74999},
-//     { product: "Apple Macbook Pro 13", price: 159999},
-//     { product: "Acer Extensa 15", price: 77999},
-//     { product: "HP Pavilion", price: 54999},
-//     { product: "Dell Intel Core i3", price: 54999},
-// ]
-
 const data = [
     { product: "Apple Macbook Air 13", price: 74999, img: "../src/assets/macbook_air.jpg"},
     { product: "Apple Macbook Pro 13", price: 159999, img: "../src/assets/macbook_pro.jpg"},
@@ -16,20 +7,3 @@ const data = [
 ]
 
 export default data
-=======
-const data = [
-  { product: "Apple Macbook Air 13", price: 74999 },
-  { product: "Apple Macbook Pro 13", price: 159999 },
-  { product: "Acer Extensa 15", price: 77999 },
-  { product: "HP Pavilion", price: 54999 },
-  { product: "Dell Intel Core i3", price: 54999 },
-];
-// const data = [
-//       { product: "Apple Macbook Air 13", price: 74999 img"../src/assets/macbook_air.jpg"},
-//       { product: "Apple Macbook Pro 13", price: 159999, img"../src/assets/macbook_pro.jpg"},
-//       { product: "Acer Extensa 15", price: 77999, img"../src/assets/acer_extensa.webp"},
-//       { product: "HP Pavilion", price: 54999, img"../src/assets/acer_extensa.jpg"},
-//       { product: "Dell Intel Core i3", price: 54999, img"../src/assets/dell.jpg"},
-//   ]
-export default data;
->>>>>>> ea70d730c0eeac744a98985b3e7e075e7913eec5
